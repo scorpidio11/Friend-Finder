0d3f7f56a62a8193bbe718b43d529f572c2d0a84
scorpidio11 <scorpido@gmail.com>
2018-10-26 22:07:30 -0700

    done!

--- server.js
+++ server.js
@@ -6,12 +6,6 @@
 var express = require("express");
 var path = require("path");
 var bodyParser = require("body-parser");
-// ==============================================================================
-// EXPRESS CONFIGURATION
-// This sets up the basic properties for our express server
-// ==============================================================================
-
-// Tells node that we are creating an "express" server
 var app = express();
 
 // Sets an initial port. We"ll use this later in our listener
@@ -24,10 +18,20 @@ app.use('/static', express.static(path.join(__dirname, 'app/public/css/')))
 
 
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
- app.use(bodyParser.text());
- app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+ app.use(bodyParser.urlencoded({ extended: true }));
+ //app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+ //app.use(bodyParser.text());
+
+
+ // parse various different custom JSON types as JSON
+app.use(bodyParser.json({ type: 'application/*+json' }))
+ 
+// parse some custom thing into a Buffer
+app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }))
+ 
+// parse an HTML body into a string
+app.use(bodyParser.text({ type: 'text/html' }))
+
 
 
 
